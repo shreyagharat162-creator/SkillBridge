999e4f13052c0185e947ed80afe690ab6b577d58
Shreya Gharat <shreyagharat162@gmail.com>
2026-09-22 09:55:55 +0530

    Allow deployed frontend CORS

--- backend/src/main/java/com/skillbridge/backend/controller/UserController.java
+++ backend/src/main/java/com/skillbridge/backend/controller/UserController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:5174"})
+@CrossOrigin(@CrossOrigin(origins = "*"))
 public class UserController {
 
     private final UserService userService;
